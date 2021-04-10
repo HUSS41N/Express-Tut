813a7383f4fd14704d1afb791b2cfca70212e93b
Md Hussain <67954546+HUSS41N@users.noreply.github.com>
2021-04-10 15:36:10 +0530

    Update index.js

--- thirdapp-restful-routing/index.js
+++ thirdapp-restful-routing/index.js
@@ -1,16 +1,30 @@
-const express = require('express')
+const express = require("express");
+const app = express();
+const path = require("path");
 const { v4: uuidv4 } = require('uuid');
-const app = express()
+const methodOverride = require('method-override')
 
-app.set('view engine','ejs')
-app.use(express.urlencoded({extended:true}))
-// app.use(express.json())
+app.set("views", (__dirname, "views"));
+app.set("view engine", "ejs");
+
+// parsing the form data
+// we have to tell express to encode the data we are recinving from req.body to get the data otherwise we will get undefined
+app.use(express.urlencoded({ extended: true }));
+
+// if we want to parse json
+app.use(express.json());
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'))
+// routes
+// app.get("/", (req, res) => {
+// 	res.render("index");
+// });
 
 // * comments 
 let comments = [
     {id:uuidv4(),username:'Hussain',comment:'Aba kadar is dope'},
     {id:uuidv4(),username:'Vivek',comment:'Aba kadar is so cool and funny'},
-    {id:uuidv4(),username:'Kudri',comment:'Aba kadar ki maa ki chut'},
+    {id:uuidv4(),username:'Kudri',comment:'Aba kadar'},
     {id:uuidv4(),username:'Saquib',comment:'ye Aba kadar kon h?'},
 ]
 // * redirect to /comments
@@ -34,7 +48,34 @@ app.post('/comments',(req,res)=>{
     comments.push({id:uuidv4(),username:username,comment:comment})
     res.redirect('/comments')
 })
+// unique comment
+app.get('/comments/:id',(req,res)=>{
+    const { id } = req.params
+    const comment = comments.find((comment)=> comment.id == id)
+    res.render('show',{comment})
+} )
+// patch request
+// patch form
+app.get('/comments/:id/edit',(req,res)=>{
+	const {id} = req.params
+	const comment = comments.find((comment)=> comment.id == id)
+	res.render('edit',{comment})
 
+})
+app.patch('/comments/:id',(req,res)=>{
+	// it has accees to req.body
+	const {id} = req.params
+	const newCommentText = req.body.comment
+	const foundComment = comments.find(comment=>comment.id === id)
+	foundComment.comment = newCommentText
+	res.redirect('/comments')
+})
+// deleting
+app.delete('/comments/:id',(req,res)=>{
+	const {id} = req.params
+	comments = comments.filter(comment=>comment.id !== id)
+	res.redirect('/comments')
+})
 app.listen(3000,()=>{
     console.log('Server started at port 3000');
 })
