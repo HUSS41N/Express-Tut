3219bc448e7a630a4f08901516bea0af99548fc4
MD Hussain <>
2021-04-20 02:37:48 +0530

    multer image upload

--- KITTEN API/index.js
+++ KITTEN API/index.js
@@ -1,15 +1,20 @@
 const express = require("express");
+const multer = require("multer")
+const path = require("path")
 const cors = require("cors");
 const mongoose = require("mongoose");
+
 const app = express();
 
 // Middlewares
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
+// exposing upload folder public to get images data easily
+app.use(express.static("upload"))
 app.use(express.json());
 app.use(cors());
 // mongoose setup
 mongoose
-	.connect("mongodb://localhost/crudAPI", {
+	.connect("mongodb://localhost/KittenAPI", {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 	})
@@ -30,10 +35,29 @@ const KittySchema = new mongoose.Schema({
 		type: Boolean,
 		default: true,
 	},
+	image:{
+		type: String,
+		required: true
+	}
 });
 // register the schema
 const Kitten = mongoose.model("Kitten", KittySchema);
 
+// multer setup
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename: (req, file, cb) => {
+        return cb(null, `${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`)
+    }
+})
+
+const upload = multer({
+    storage: storage,
+    limits: {
+        fileSize: 100000
+    }
+})
+app.use('/image', express.static('upload/images'));
 // routes
 app.get("/", (req, res, next) => {
 	res.redirect("/kittens");
@@ -46,10 +70,18 @@ app.get("/kittens", (req, res, next) => {
 		.catch((err) => res.json({ msg }));
 });
 
-app.post("/kittens", (req, res, next) => {
-	const { name, breed, isAvailable } = req.body;
-	console.log(name, breed, isAvailable);
-	Kitten.create({ name, breed, isAvailable })
+app.post("/kittens",upload.single('image'), (req, res, next) => {
+	console.log(req.file)
+	console.log(req.file.path)
+	const obj = {
+        name: req.body.name,
+        breed: req.body.breed,
+		isAvailable: req.body.isAvailable,
+        image: req.file.path
+    }
+	// const { name, breed, isAvailable } = req.body;
+	// console.log(req.file)
+	Kitten.create(obj)
 		.then((created) => res.json({ created }))
 		.catch((err) => res.redirect("/"));
 });
@@ -68,6 +100,7 @@ app.put("/kittens/:id", (req, res, next) => {
 		.then((updated) => res.json({ updated }))
 		.catch((err) => console.log(err));
 });
+
 app.delete("/kittens/:id", (req, res, next) => {
 	const { id } = req.params;
 	Kitten.findByIdAndDelete(id)
@@ -75,10 +108,20 @@ app.delete("/kittens/:id", (req, res, next) => {
 		.catch((err) => console.log(err));
 });
 
-app.get("*", (req, res, next) => {
-	res.redirect("/");
-});
+// app.get("*", (req, res, next) => {
+// 	res.redirect("/");
+// });
 
+
+function errHandler(err, req, res, next) {
+    if (err instanceof multer.MulterError) {
+        res.json({
+            success: 0,
+            message: err.message
+        })
+    }
+}
+app.use(errHandler);
 app.listen(3000, () => {
 	console.log("Server started at http://localhost:3000/");
 });
